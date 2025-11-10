b9183b99d944beaea5df892b8f28f8d2bc1ad9d0
alex <766f6964+github@gmail.com>
2025-11-10 21:25:08 +0100

    warnings fixed (php-stan)

--- src/Wikipedia/File.php
+++ src/Wikipedia/File.php
@@ -9,9 +9,9 @@ class File extends \aportela\MediaWikiWrapper\API
     public const REST_API_FILE_GET = "https://commons.wikimedia.org/w/rest.php/v1/file/File:%s";
 
     protected ?\aportela\MediaWikiWrapper\FileInformation $prefered = null;
-    
+
     protected ?\aportela\MediaWikiWrapper\FileInformation $original = null;
-    
+
     protected ?\aportela\MediaWikiWrapper\FileInformation $thumbnail = null;
 
     public function __construct(\Psr\Log\LoggerInterface $logger, int $throttleDelayMS = self::DEFAULT_THROTTLE_DELAY_MS, ?\aportela\SimpleFSCache\Cache $cache = null)
@@ -25,50 +25,50 @@ class File extends \aportela\MediaWikiWrapper\API
         if (
             isset($json->preferred) &&
             is_object($json->preferred) &&
-            isset($json->preferred->mediatype) &&
-            isset($json->preferred->url)
+            isset($json->preferred->mediatype) && is_string($json->preferred->mediatype) &&
+            isset($json->preferred->url) && is_string($json->preferred->url)
         ) {
             $this->prefered = new \aportela\MediaWikiWrapper\FileInformation(
                 \aportela\MediaWikiWrapper\FileInformationType::PREFERRED,
                 $json->preferred->mediatype,
-                $json->preferred->size ?? null,
-                $json->preferred->width ?? null,
-                $json->preferred->height ?? null,
-                $json->preferred->duration ?? null,
+                isset($json->preferred->size) && is_numeric($json->preferred->size) ? intval($json->preferred->size) : null,
+                isset($json->preferred->width) && is_numeric($json->preferred->width) ? intval($json->preferred->width) : null,
+                isset($json->preferred->height) && is_numeric($json->preferred->height) ? intval($json->preferred->height) : null,
+                isset($json->preferred->duration) && is_numeric($json->preferred->duration) ? intval($json->preferred->duration) : null,
                 $json->preferred->url
             );
         }
-        
+
         if (
             isset($json->original) &&
             is_object($json->original) &&
-            isset($json->original->mediatype) &&
-            isset($json->original->url)
+            isset($json->original->mediatype) && is_string($json->original->mediatype) &&
+            isset($json->original->url) && is_string($json->original->url)
         ) {
             $this->original = new \aportela\MediaWikiWrapper\FileInformation(
                 \aportela\MediaWikiWrapper\FileInformationType::ORIGINAL,
                 $json->original->mediatype,
-                $json->original->size ?? null,
-                $json->original->width ?? null,
-                $json->original->height ?? null,
-                $json->original->duration ?? null,
+                isset($json->original->size) && is_numeric($json->original->size) ? intval($json->original->size) : null,
+                isset($json->original->width) && is_numeric($json->original->width) ? intval($json->original->width) : null,
+                isset($json->original->height) && is_numeric($json->original->height) ? intval($json->original->height) : null,
+                isset($json->original->duration) && is_numeric($json->original->duration) ? intval($json->original->duration) : null,
                 $json->original->url
             );
         }
-        
+
         if (
             isset($json->thumbnail) &&
             is_object($json->thumbnail) &&
-            isset($json->thumbnail->mediatype) &&
-            isset($json->thumbnail->url)
+            isset($json->thumbnail->mediatype) && is_string($json->thumbnail->mediatype) &&
+            isset($json->thumbnail->url) && is_string($json->thumbnail->url)
         ) {
             $this->thumbnail = new \aportela\MediaWikiWrapper\FileInformation(
                 \aportela\MediaWikiWrapper\FileInformationType::THUMBNAIL,
                 $json->thumbnail->mediatype,
-                $json->thumbnail->size ?? null,
-                $json->thumbnail->width ?? null,
-                $json->thumbnail->height ?? null,
-                $json->thumbnail->duration ?? null,
+                isset($json->thumbnail->size) && is_numeric($json->thumbnail->size) ? intval($json->thumbnail->size) : null,
+                isset($json->thumbnail->width) && is_numeric($json->thumbnail->width) ? intval($json->thumbnail->width) : null,
+                isset($json->thumbnail->height) && is_numeric($json->thumbnail->height) ? intval($json->thumbnail->height) : null,
+                isset($json->thumbnail->duration) && is_numeric($json->thumbnail->duration) ? intval($json->thumbnail->duration) : null,
                 $json->thumbnail->url
             );
         }
@@ -110,24 +110,24 @@ class File extends \aportela\MediaWikiWrapper\API
                 if ($this->prefered != null) {
                     $url = $this->prefered->url;
                 }
-                
+
                 break;
             case \aportela\MediaWikiWrapper\FileInformationType::ORIGINAL:
                 if ($this->original != null) {
                     $url = $this->original->url;
                 }
-                
+
                 break;
             case \aportela\MediaWikiWrapper\FileInformationType::THUMBNAIL:
                 if ($this->thumbnail != null) {
                     $url = $this->thumbnail->url;
                 }
-                
+
                 break;
             default:
                 throw new \InvalidArgumentException("Invalid informationType");
         }
-        
+
         return ($url);
     }
 }
